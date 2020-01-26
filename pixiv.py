396a2badbaa8831824e7cbef88034f861ffbc980
Tina <me@tina.moe>
2020-01-26 06:37:07 +0100

    Remove test pixiv.py

--- pixiv.py
+++
@@ -1,9 +0,0 @@
-from pixivpy3 import PixivAPI
-import json
-
-api = PixivAPI()
-api.login('shookaite@gmail.com', 'SQarX9pb')
-# data = api.works(79102155)
-data = api.me_following_works()
-with open('sample_following.json', 'w') as f:
-    json.dump(data, f, indent=4)
